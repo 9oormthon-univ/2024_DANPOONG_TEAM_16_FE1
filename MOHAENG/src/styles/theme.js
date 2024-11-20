e7dc3cf6f18cd26ac647ca5680f4a93b24faed1e
daahyunk <ekgusdl0408@naver.com>
2024-11-21 00:23:29 +0900

    📝 Theme 컬러 코드 추가

---
+++ MOHAENG/src/styles/theme.js
@@ -0,0 +1,18 @@
+const theme = {
+    color: {
+        white: '#FFFFFF',
+        black: '#000000',
+        primary: '#0A70C9',
+        blue1: '#BDE5FF',
+        blue2: '#17A1FA',
+        gray1: '#F3F3F3',
+        gray2: '#6E6E6E',
+        gray3: '#DFDFDF',
+        gray4: '#595959',
+    },
+    font: {
+        pretendard: `'Pretendard', sans-serif`,
+    },
+};
+
+export default theme;
