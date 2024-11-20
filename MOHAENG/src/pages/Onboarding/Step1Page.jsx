6947702e41d84dc05b01756b6d402e4be7b324f1
daahyunk <ekgusdl0408@naver.com>
2024-11-21 00:22:13 +0900

    ✨ 온보딩 스텝1 페이지 컴포넌트 구현 (#1)

---
+++ MOHAENG/src/pages/Onboarding/Step1Page.jsx
@@ -0,0 +1,11 @@
+import Step1 from '../../components/onboarding/Step1'; // 최상위 컴포넌트
+
+const Step1Page = () => {
+    return (
+        <>
+            <Step1 />
+        </>
+    );
+};
+
+export default Step1Page;
