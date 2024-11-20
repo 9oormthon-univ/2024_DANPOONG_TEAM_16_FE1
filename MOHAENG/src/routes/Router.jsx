e8c0338b314d4fe69ce4e6baecb0e5658d424215
daahyunk <ekgusdl0408@naver.com>
2024-11-21 04:50:27 +0900

    ✨ 온보딩 스텝3 라우터 추가

--- MOHAENG/src/routes/Router.jsx
+++ MOHAENG/src/routes/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 
 import Step1 from '../pages/Onboarding/Step1Page';
 import Step2 from '../pages/Onboarding/Step2Page';
+import Step3 from '../pages/Onboarding/Step3Page';
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
   {
     path: '/onboarding-step2', 
     element: <Step2 />,
+  },
+  {
+    path: '/onboarding-step3',
+    element: <Step3 />,
   },
 ]);
 
