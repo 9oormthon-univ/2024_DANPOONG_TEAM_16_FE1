74aa329ac67015553c744e575ae1095b2ca94cb3
daahyunk <ekgusdl0408@naver.com>
2024-11-21 01:17:33 +0900

    ✨ Step2 라우터 추가

--- MOHAENG/src/routes/Router.jsx
+++ MOHAENG/src/routes/Router.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import Step1 from '../pages/Onboarding/Step1Page';
+import Step2 from '../pages/Onboarding/Step2Page';
 
 const router = createBrowserRouter([
   {
     path: '/onboarding-step1',
     element: <Step1 />,
+  },
+  {
+    path: '/onboarding-step2', 
+    element: <Step2 />,
   },
 ]);
 
