c395779e325b969f80c27839ba2548a847e4289a
daahyunk <ekgusdl0408@naver.com>
2024-11-21 00:20:23 +0900

    ✨ 장애 아이콘 및 온보딩 헤더 화살표 svgr 추가

---
+++ MOHAENG/src/assets/svg/index.js
@@ -0,0 +1,5 @@
+export { default as IcBlind } from "./IcBlind";
+export { default as IcDeaf } from "./IcDeaf";
+export { default as IcElderly } from "./IcElderly";
+export { default as IcInfant } from "./IcInfant";
+export { default as IcWheelchair } from "./IcWheelchair";
