a975691a1800ff1879c74d8b8d4f7e59327a4f51
daahyunk <ekgusdl0408@naver.com>
2024-11-21 01:42:38 +0900

    ✨ 지역 선택 버튼 컴포넌트 구현

---
+++ MOHAENG/src/components/onboarding/RegionSelector/RegionButton.style.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import theme from '../../../styles/theme';
+
+export const Button = styled.button.withConfig({
+    shouldForwardProp: (prop) => prop !== 'isSelected',
+})`
+    width: 10.4rem;
+    height: 4.8rem;
+    border: 1.5px solid
+        ${({ isSelected }) => (isSelected ? theme.color.primary : theme.color.gray6)};
+    background-color: ${({ isSelected }) =>
+        isSelected ? theme.color.blue1 : theme.color.white};
+    color: ${({ isSelected }) =>
+        isSelected ? theme.color.black : theme.color.gray7};
+    border-radius: 37px;
+    cursor: pointer;
+    font-size: 1.7rem;
+    font-weight: ${(props) => (props.isSelected ? 600 : 400)};
+
+    &:hover {
+        border-color: ${theme.color.blue2};
+    }
+`;
