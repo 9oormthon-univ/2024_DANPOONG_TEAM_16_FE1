3e3729daa95428f5ca875fdd0cac873440808dbb
daahyunk <ekgusdl0408@naver.com>
2024-11-21 20:03:53 +0900

    ✨ 온보딩 스텝4 페이지 컴포넌트 구현

---
+++ MOHAENG/src/pages/Onboarding/Step4Page.jsx
@@ -0,0 +1,11 @@
+import Step4 from '../../components/onboarding/Step4';
+
+const Step4Page = () => {
+    return (
+        <>
+            <Step4 />
+        </>
+    );
+}
+
+export default Step4Page
