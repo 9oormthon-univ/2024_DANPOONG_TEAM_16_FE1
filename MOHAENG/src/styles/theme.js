f2cca6bbb1a84ef8764b832bbe3b28a7e447d8be
daahyunk <ekgusdl0408@naver.com>
2024-11-22 18:07:22 +0900

    📝 Theme.js에 컬러코드 추가

--- MOHAENG/src/styles/theme.js
+++ MOHAENG/src/styles/theme.js
@@ -14,6 +14,10 @@ const theme = {
         gray6: '#E3E3E3',
         gray7: '#676767',
         gray8: '#858585',
+        gray9: '#2E2E2E',
+        gray10: '#f1f1f1',
+        gray11: '#F0F0F0',
+        gray12: '#525252',
     },
     font: {
         pretendard: `'Pretendard', sans-serif`,
