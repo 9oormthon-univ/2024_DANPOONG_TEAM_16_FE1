575d049fc5046410be527f2d8f795a4d8643905b
daahyunk <ekgusdl0408@naver.com>
2024-11-22 18:08:50 +0900

    ✨ 시설물 정보 컴포넌트 구현

---
+++ MOHAENG/src/components/PlaceDetail/FacilityIcons/FacilityIcon.style.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+import theme from '../../../styles/theme';
+
+export const Wrapper = styled.div`
+    width: 100%;
+    margin-top: 2.4rem;
+`;
+
+export const Title = styled.h2`
+    font-size: 2rem;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 150%; 
+    letter-spacing: -0.28px;
+    margin-left: 1.7rem;
+    color: ${theme.color.black};
+`;
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    gap: 3.5rem;
+    margin-top: 1.1rem;
+`;
+ 
+export const IconWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 3.1rem;
+    height: 3.8rem;
+
+    svg {
+        width: 100%;
+        height: 100%;
+    }
+`;
