770bfa8e6955167cbf572cad3f4ae142e85003fd
daahyunk <ekgusdl0408@naver.com>
2024-11-22 21:26:55 +0900

    ✨ 뒤로 가기 버튼 및 홈 버튼 구현

--- MOHAENG/src/components/PlaceDetail/PlaceDetailHeader/PlaceDetailHeader.jsx
+++ MOHAENG/src/components/PlaceDetail/PlaceDetailHeader/PlaceDetailHeader.jsx
@@ -1,15 +1,27 @@
 import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 import * as S from './PlaceDetailHeader.style';
 
 import { IcPlacedetailHeaderBack, IcPlacedetailHeaderHome } from '../../../assets/svg';
 
-const PlaceDetailHeader = ({ onBackClick, onHomeClick }) => {
+const PlaceDetailHeader = () => {
+    const navigate = useNavigate();
+
+    const handleBackClick = () => {
+        navigate(-1); 
+    };
+
+    const handleHomeClick = () => {
+        navigate('/'); // 홈으로 이동
+        console.log('iOS 홈 화면과 연동 필요'); // 추후 iOS와 연동 시 수정
+    };
+
     return (
         <S.HeaderContainer>
-            <S.IconButton onClick={onBackClick}>
+            <S.IconButton onClick={handleBackClick}>
                 <IcPlacedetailHeaderBack />
             </S.IconButton>
-            <S.IconButton onClick={onHomeClick}>
+            <S.IconButton onClick={handleHomeClick}>
                 <IcPlacedetailHeaderHome />
             </S.IconButton>
         </S.HeaderContainer>
@@ -17,8 +29,8 @@ const PlaceDetailHeader = ({ onBackClick, onHomeClick }) => {
 };
 
 PlaceDetailHeader.propTypes = {
-    onBackClick: PropTypes.func.isRequired,
-    onHomeClick: PropTypes.func.isRequired,
+    onBackClick: PropTypes.func,
+    onHomeClick: PropTypes.func,
 };
 
 export default PlaceDetailHeader;
