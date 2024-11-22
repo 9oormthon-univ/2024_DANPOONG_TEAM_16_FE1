70d56a994783e78dde542be2723706ac16a3d6bf
daahyunk <ekgusdl0408@naver.com>
2024-11-22 18:09:20 +0900

    ✨ PlaceImage 컴포넌트 구현

---
+++ MOHAENG/src/components/PlaceDetail/PlaceImage/PlaceImage.style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const ImageWrapper = styled.div`
+    width: 100%;
+    height: 29.3rem;
+    overflow: hidden;
+`;
+
+export const Image = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover; 
+    object-position: center;
+`;
